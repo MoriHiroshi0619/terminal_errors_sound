41265b6731fa830ed6661a175dd4ae82f027196b
MoriHiroshiDesu <anderson.hio.c.m@gmail.com>
2026-06-01 22:48:51 -0400

    fix: suporte ao ble.sh no bash.sh

--- shell/bash.sh
+++ shell/bash.sh
@@ -3,11 +3,10 @@ export LAST_ERROR_SOUND=""
 terminal_error_sound() {
     local ret=$?
 
-    [ "$ret" -eq 0 ] && return
+    (( ret == 0 )) && return
 
     local dir="$HOME/.terminal-error-sounds/sounds"
 
-    # Suporta mp3, wav e ogg (conforme especificações futuras)
     mapfile -t sounds < <(
         find "$dir" \
             -maxdepth 1 \
@@ -15,19 +14,24 @@ terminal_error_sound() {
             \( -name '*.mp3' -o -name '*.wav' -o -name '*.ogg' \)
     )
 
-    [ ${#sounds[@]} -eq 0 ] && return
+    (( ${#sounds[@]} == 0 )) && return
 
     local sound="${sounds[$((RANDOM % ${#sounds[@]}))]}"
 
-    # Garante que o próximo som seja diferente do último
-    if [ ${#sounds[@]} -gt 1 ]; then
-        while [ "$sound" = "$LAST_ERROR_SOUND" ]; do
+    if (( ${#sounds[@]} > 1 )); then
+        while [[ "$sound" == "$LAST_ERROR_SOUND" ]]; do
             sound="${sounds[$((RANDOM % ${#sounds[@]}))]}"
         done
     fi
+
     LAST_ERROR_SOUND="$sound"
 
     (paplay "$sound" >/dev/null 2>&1 &) >/dev/null 2>&1
 }
 
-PROMPT_COMMAND="terminal_error_sound"
+
+if declare -F blehook >/dev/null; then
+    blehook POSTEXEC+=terminal_error_sound
+else
+    PROMPT_COMMAND+=(terminal_error_sound)
+fi
